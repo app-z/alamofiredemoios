fd8cbabe261c2e22aa0c02e4940cbc2639d24389
Dmitry <dmitry@andreev.atwss.com>
2018-10-25 17:37:35 +0300

    Initial Commit

---
+++ AlamofireDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireDemo
+//
+//  Created by Dmitry on 10/25/18.
+//  Copyright © 2018 Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
